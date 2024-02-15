f9056e7bd0db347622cde79868553b98b6bc3bbf
nour-derwich <nourderouich159@gmail.com>
2024-02-15 22:38:07 +0100

    index page is done

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var cityAlert = (element) => {
+    window.alert('Laoding weather report...!!!')
+}
+var cookieHid = (element) => {
+    element.style.visibility  = 'hidden';
+}
+
+var cookie = document.querySelector('#cookie');
+
+function deleteCookie(){
+    cookie.remove()
+}
